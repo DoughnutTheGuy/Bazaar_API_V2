fa2088fb461495f5f914a8be1ef54b2f0d531056
Jonas232005 <jonas232005@gmail.com>
2020-03-22 21:00:00 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+
+var getJSON = require('get-json')
+
+var API_KEY = "eca6145a-32b5-43f1-99d0-6adb7bcfc690"
+
+var url = "https://api.hypixel.net/skyblock/bazaar/product?key=" + API_KEY +"&productId=";
+
+router.get("/products/:product_id", (req, res) => {
+    getJSON(url+req.params.product_id, function(error, response){
+        res.json(response);
+    });
+});
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app);
